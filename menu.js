03ec408b22b527cfe88d928e06d73894fc660376
brigerton <jrhallen@outlook.com>
2019-11-22 15:51:31 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 body {
   font-family: Arial, Helvetica, sans-serif;
@@ -58,8 +53,6 @@ body {
   display: block;
 }
 </style>
-</head>
-<body>
 
 <div class="navbar">
   <a href="#home">Home</a>
@@ -85,6 +78,3 @@ body {
   <h3>Input info here</h3>
   <p>And over here</p>
 </div>
-
-</body>
-</html>
